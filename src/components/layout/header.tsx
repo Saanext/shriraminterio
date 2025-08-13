993665ce604420a3c3da810ef7c5d49068aa92f2
Deepak Bagada <artechway@gmail.com>
2025-08-13 14:09:29 +0000

    don not be transparent after the scroll

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import Link from 'next/link';
@@ -21,10 +20,10 @@ export function Header() {
   }
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-background">
       <div className="container flex h-16 items-center">
         <Link href="/" className="mr-6 flex items-center space-x-2">
-          <Image src="/shriram.png" alt="Shriram Interio Logo" width={150} height={50} className="object-contain" data-ai-hint="company logo" />
+          <Image src="/company.png" alt="Shriram Interio Logo" width={150} height={50} className="object-contain" data-ai-hint="company logo" />
         </Link>
 
         <nav className="hidden md:flex items-center space-x-4 text-sm font-medium">
