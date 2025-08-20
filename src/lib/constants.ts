c99b3970588e3928fa2c204b2bed306b1f016134
Deepak Bagada <artechway@gmail.com>
2025-08-20 18:37:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 import type { LucideIcon } from 'lucide-react';
-import { GalleryHorizontal, GanttChartSquare, Home, Info, Layers, Users, Wrench, Phone, FileText, CalendarPlus } from 'lucide-react';
+import { GalleryHorizontal, GanttChartSquare, Home, Info, Layers, Users, Wrench, Phone, CalendarPlus } from 'lucide-react';
 
 export type NavItem = {
   label: string;
