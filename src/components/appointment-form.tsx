2cfe0d42eec329e0aad5221a24cf8a33a8aaef73
Deepak Bagada <artechway@gmail.com>
2025-08-28 11:05:32 +0000

    make sure data must be  liek tomorrow not a current data

--- src/components/appointment-form.tsx
+++ src/components/appointment-form.tsx
@@ -233,7 +233,7 @@ export function AppointmentForm() {
                                             </FormControl>
                                         </PopoverTrigger>
                                         <PopoverContent className="w-auto p-0" align="start">
-                                            <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date < new Date(new Date().setDate(new Date().getDate() - 1))} initialFocus />
+                                            <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date <= new Date()} initialFocus />
                                         </PopoverContent>
                                     </Popover>
                                     <FormMessage />
